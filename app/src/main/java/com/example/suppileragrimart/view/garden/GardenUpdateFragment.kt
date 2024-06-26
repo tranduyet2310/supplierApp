47127697a20f0e1db03aec6bf2ef12b9aea6856c
tranduyet2310 <trantheduyetsl@gmail.com>
2024-06-26 13:44:47 +0700

    update layout

--- app/src/main/java/com/example/suppileragrimart/view/garden/GardenUpdateFragment.kt
+++ app/src/main/java/com/example/suppileragrimart/view/garden/GardenUpdateFragment.kt
@@ -3,7 +3,6 @@ package com.example.suppileragrimart.view.garden
 import android.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
